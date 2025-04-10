0d7c36996cd75cc95e783c7b792051c5fa295cd1
Ryan Mcvey <rjmcvey2024@gmail.com>
2025-04-10 11:44:45 +0000

    Update launch configuration and modify API root view
    - Added a new configuration to launch the Django server in launch.json.
    - Updated ALLOWED_HOSTS in settings.py to restrict access to specific hosts.
    - Enhanced api_root view to handle POST requests and return a message.
    - Updated response URLs in api_root to include the base URL for API endpoints.

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-1=7e&q5*@yas$kk=v3&y$rr5btse02def7qly1^a7%drli9_63
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'curly-dollop-jpp9vv55j59hpwwx-8000.app.github.dev']
 
 
 # Application definition
