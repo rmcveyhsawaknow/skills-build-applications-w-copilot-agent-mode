5a67d28b17f26a3048f51cbe098de76655cf1565
Ryan Mcvey <rjmcvey2024@gmail.com>
2025-04-10 11:44:29 +0000

    Remove CORS headers from installed apps and middleware

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -37,14 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'corsheaders',
     'rest_framework',
     'djongo',
     'octofit_tracker',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
